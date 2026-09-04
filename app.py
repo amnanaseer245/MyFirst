8b22698d8fbb031b4df0e479d14039d5dee47642
amnanaseer245 <amnanaseer245@gmail.com>
2026-09-05 00:11:23 +0500

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,6 @@ import google.generativeai as genai
 GOOGLE_API_KEY = st.secrets["GOOGLE_API_KEY"]
 genai.configure(api_key=GOOGLE_API_KEY)
 
-
-model = genai.GenerativeModel('gemini-1.5-flash')
-
 st.title("🎓 Education AI Student Assistant")
 st.write("You can ask your academic questions here!")
 
@@ -19,6 +16,8 @@ student_query = st.text_area("Your Question or Request:")
 if st.button("Submit Request"):
     if student_name and student_query:
         with st.spinner("Processing..."):
+      
+            model = genai.GenerativeModel('gemini-1.5-flash')
             response = model.generate_content(student_query)
             st.success("Your answer is ready!")
             st.write(response.text)
